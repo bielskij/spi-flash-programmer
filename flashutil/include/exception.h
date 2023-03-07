74cdc3bee182b606ab450d70f57237371170f2fa
Jarosław Bielski <bielski.j@gmail.com>
2023-03-07 22:04:48 +0100

    Added exception class

---
+++ flashutil/include/exception.h
@@ -0,0 +1,29 @@
+/*
+ * exception.h
+ *
+ *  Created on: 7 mar 2023
+ *      Author: Jaroslaw Bielski (bielski.j@gmail.com)
+ */
+
+#ifndef EXCEPTION_H_
+#define EXCEPTION_H_
+
+#include <stdexcept>
+#include <string>
+
+
+class Exception : public std::runtime_error {
+	public:
+		Exception(const std::string &msg) : std::runtime_error(msg) {
+
+		}
+
+		Exception(const std::string &file, int line, const std::string &msg) :
+			std::runtime_error(file + ":" + std::to_string(line) + " " + msg)
+		{
+		}
+};
+
+#define throw_Exception(msg) do { throw Exception(__FILE__, __LINE__, msg); } while (0);
+
+#endif /* EXCEPTION_H_ */
