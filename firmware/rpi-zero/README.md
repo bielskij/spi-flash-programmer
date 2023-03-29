5dc9340b8f97e6de5ee4823873736710df7b72c9
Jaroslaw Bielski <bielski.j@gmail.com>
2023-03-29 13:25:19 +0200

    Basic USB CDC support

--- firmware/rpi-zero/README.md
+++ firmware/rpi-zero/README.md
@@ -1,1 +1,16 @@
+  * OS environment preparation
+  
+```
 sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
+```
+
+  * Project initialization
+  
+```
+git submodule init
+git submodule update
+cd firmware/rpi-zero/pico-sdk
+git submodule update --init
+```
+
+s
--- firmware/rpi-zero/src/main.c
+++ firmware/rpi-zero/src/main.c
@@ -6,10 +6,35 @@
  */
 
 #include <stdio.h>
-#include "pico/stdlib.h"
+
+#include "hardware/spi.h"
+
+#include "tusb.h"
+
+#define CDC_CHANNEL 0
+
+static void _cdcTask(void) {
+	if (tud_cdc_n_available(CDC_CHANNEL)) {
+		uint8_t buf[CFG_TUD_CDC_EP_BUFSIZE];
+
+		uint32_t count = tud_cdc_n_read(CDC_CHANNEL, buf, sizeof(buf));
+
+//		tud_cdc_n_write_str(CDC_CHANNEL, "A!\r\n");
+//		tud_cdc_n_write(CDC_CHANNEL, buf, count);
+//		tud_cdc_n_write_flush(CDC_CHANNEL);
+	}
+}
+
 
 int main() {
-	stdio_init_all();
+	board_init();
+	tusb_init();
+
+	while (1) {
+		tud_task();
+
+		_cdcTask();
+	}
 
 	return 0;
 }
