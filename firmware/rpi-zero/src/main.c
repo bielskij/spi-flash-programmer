b0889340a6fc4634fc7fb69ed412d1501e2d6b63
Jaroslaw Bielski <bielski.j@gmail.com>
2023-03-29 08:00:42 +0200

    Initial commit

---
+++ firmware/rpi-zero/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 29 mar 2023
+ *      Author: bielski.j@gmail.com
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+	stdio_init_all();
+
+	return 0;
+}
