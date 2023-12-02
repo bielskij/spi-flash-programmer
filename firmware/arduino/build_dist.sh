ecaa86e335f21d365da9277264a0ece28ddf7749
Jarosław Bielski <bielski.j@gmail.com>
2023-12-03 00:03:57 +0100

    Added dist images

--- firmware/arduino/build_dist.sh
+++ firmware/arduino/build_dist.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 VERBOSE=0
 
 function build() {
@@ -7,15 +11,19 @@ function build() {
 	local baud=$1; shift
 	local outDir=$1; shift
 
-	make V=${VERBOSE} F_CPU=${fcpu} BAUD=${baud} -f Makefile clean all
-	
-	cp -f out/firmware.hex ${outDir}/firmware_$(( fcpu / 1000000 ))mhz_${baud}bps.hex
+	rm -rf build; mkdir build
+	pushd build
+		cmake ../ -DFW_F_CPU=${fcpu} -DFW_BAUD=${baud}
+		VERBOSE=${VERBOSE} make -j8 clean all
+		cp -f firmware.hex ${outDir}/firmware_$(( fcpu / 1000000 ))mhz_${baud}bps.hex
+	popd
+	rm -rf build;
 }
 
-build 16000000  500000 dist
-build 16000000   38400 dist
-build 16000000   19200 dist
+build 16000000  500000 ${SCRIPT_DIR}/dist
+build 16000000   38400 ${SCRIPT_DIR}/dist
+build 16000000   19200 ${SCRIPT_DIR}/dist
 
-build  8000000  500000 dist
-build  8000000   38400 dist
-build  8000000   19200 dist
+build  8000000  500000 ${SCRIPT_DIR}/dist
+build  8000000   38400 ${SCRIPT_DIR}/dist
+build  8000000   19200 ${SCRIPT_DIR}/dist
